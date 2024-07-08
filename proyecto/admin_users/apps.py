6fe3ab37efc4d2cb0c75e675ba229cf33b3084c8
“Omargeimer” <omargeimer2122@gmail.com>
2024-07-08 08:13:11 -0600

    Agregando Models y Panel de Administración

--- proyecto/admin_users/apps.py
+++ proyecto/admin_users/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AdminUsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'admin_users'
+    verbose_name = 'Administrar Usuarios'
