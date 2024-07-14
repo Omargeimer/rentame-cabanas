033bfb245479316eb2550f1f19f46651021b8020
DieNava <dnava0282@gmail.com>
2024-07-13 19:13:10 -0600

    Actualización Admin

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inicio',
+    'proyecto',
     'admin_cabanas.apps.AdminCabanasConfig',
     'admin_users.apps.AdminUsersConfig',
     'cabanas',
@@ -47,7 +49,11 @@ INSTALLED_APPS = [
     'renta.apps.RentaConfig',
     'valoraciones.apps.ValoracionesConfig',
     'ckeditor',
+    'colorfield',
     ]
+
+X_FRAME_OPTIONS='SAMEORIGIN'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
