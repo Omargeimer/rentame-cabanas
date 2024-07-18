68c4378c4ae4cfb55dd2b4e30fb34358b9718e61
Nolajoda1 <omargeimer2122@gamil.com>
2024-07-18 12:09:34 -0600

    Encriptación contraseña Admin

--- proyecto/admin_users/models.py
+++ proyecto/admin_users/models.py
@@ -4,4 +4,8 @@ from django.db import models
 class Usuario(AbstractUser):
     telefono = models.CharField(null=True,max_length=10)
     imagen = models.ImageField(null=True, upload_to="imgUsers", verbose_name="Imagen") 
-
+    
+    def save(self, *args, **kwargs):
+        if not self.pk or 'password' in kwargs:
+            self.set_password(self.password)
+        super().save(*args, **kwargs)
