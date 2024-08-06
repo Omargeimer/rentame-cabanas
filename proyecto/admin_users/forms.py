e4ee8942ddc32cf13ada69526639e43b9f1c8133
Liz427 <ramirezk187@gmail.com>
2024-08-06 17:41:45 -0600

    Alertas

--- proyecto/admin_users/forms.py
+++ proyecto/admin_users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from admin_users.models import Usuario
+from django.contrib.auth.forms import AuthenticationForm
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
